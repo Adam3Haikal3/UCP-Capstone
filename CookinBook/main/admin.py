853d6c2005cb00cf8ee7d8c7ae0ed3382204fef5
Adam3Haikal3 <haikal.8@osu.edu>
2026-02-07 18:10:06 -0500

    chore: format with ruff

--- CookinBook/main/admin.py
+++ CookinBook/main/admin.py
@@ -2,13 +2,22 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
+
 class AllUsersAdmin(UserAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs
-    
-    list_filter = ('is_active', 'is_superuser', 'groups')
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+
+    list_filter = ("is_active", "is_superuser", "groups")
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+    )
+
 
 # Unregister the default User admin
 admin.site.unregister(User)
