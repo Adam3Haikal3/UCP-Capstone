87287ac98f4c0ce66e80e35fa64cd40c4cb05cf5
Andy Yu <yu.3428@osu.edu>
2026-02-26 14:17:02 -0500

    added three 3 tests for home, login, and signup pages

--- CookinBook/main/tests.py
+++ CookinBook/main/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+from django.test import TestCase
 
 # Create your tests here.
+class SimplePagesTest(TestCase):
+    def test_home_page_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_returns_200(self):
+        response = self.client.get("/login/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_returns_200(self):
+        response = self.client.get("/signup/")
+        self.assertEqual(response.status_code, 200)
